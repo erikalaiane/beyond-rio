5ea3a0a691b68db18ad9537532e52edfc498d703
erikalaiane <>
2025-05-15 20:06:36 -0300

    commit base archives

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blogs from './pages/Blogs';
+import BlogsDetails from './pages/BlogsDetails';
+import Destinos from './pages/Destinos';
+import NoPage from './pages/NoPage';
 
 const App = () => {
   return (
-    <div>pão de queijo</div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/blogs' element={<Blogs />} />
+            <Route path='/blogs/:id' element={<BlogsDetails />} />
+            <Route path='/places' element={<Destinos />} />
+            <Route path='*' element={<NoPage />} />
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+     <div className="fixed top-0 right-0 w-full bg-white text-black shadow-md">
+      <div className="container">
+
+      </div>
+     </div>
+    </>
   )
 };
 
