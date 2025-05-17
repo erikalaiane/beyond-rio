3b3cadab09cf871d81af017b55167446c58fe316
erikalaiane <>
2025-05-16 21:35:36 -0300

    hero section start

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="bg-black/20 h-full">
+        <div className="h-full flex justify-center items-center p-4 bg-blue-950/10">
+            <div className="container grid grid-cols-1 gap-4">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+};
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import MainVid from "../assets/main.mp4";
 
 function Home() {
   return (
     <>
       <div>
-        <div>
-          
+        <div className='relative h-[700px]'>
+          <video 
+           autoPlay loop muted
+           className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]">
+
+            <source src={MainVid} type="video/mp4"/>
+          </video>
+          <Home />
         </div>
       </div>
     </>
